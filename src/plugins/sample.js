ebaaaec7272ac4b81f3435b2a74537ef8eb94f7c
Peter Kuijpers <oeps46@gmail.com>
2018-02-05 01:53:08 +0100

    initial commit

---
+++ src/plugins/sample.js
@@ -0,0 +1,8 @@
+
+module.exports = function(bot) {
+    // Modify bot here
+    // bot.config // Music-bot Config object
+    // bot.client // discord.js client
+    // bot.queue // Music Queue
+    //console.log(bot);
+};
